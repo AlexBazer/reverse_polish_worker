bb4c12f3ae4bc576e6bd9368d23bb4d9cea08a8b
Alexandr Balagura <davnobezimeni@gmail.com>
2017-03-02 06:47:07 +0200

    Complete reverce polish notation calculator for simple +-/* and update tests

--- calculate.go
+++ calculate.go
@@ -1,6 +1,69 @@
 package main
 
-func calculate(expression string) float64 {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-	return 10
+func pop(a []float32) (float32, []float32, error) {
+	l := len(a)
+	if l == 0 {
+		return 0, a, fmt.Errorf("Expression is not compnete and can't be parsed")
+	}
+	return a[l-1], a[:l-1], nil
+}
+
+func apply(s string, stack []float32) ([]float32, error) {
+	var a, b, res float32
+	var errA, errB error
+	a, stack, errA = pop(stack)
+	b, stack, errB = pop(stack)
+	if errA != nil {
+		return stack, errA
+	} else if errB != nil {
+		return stack, errB
+	}
+
+	switch {
+	case s == "+":
+		res = b + a
+	case s == "-":
+		res = b - a
+	case s == "*":
+		res = b * a
+	case s == "/":
+		res = b / a
+	}
+	stack = append(stack, res)
+	return stack, nil
+}
+
+func calculate(expression string) (float32, error) {
+	var splited = strings.Split(expression, " ")
+	var stack = make([]float32, 0)
+	var err error
+	for _, s := range splited {
+		switch s {
+		case "+",
+			"-",
+			"*",
+			"/":
+			stack, err = apply(s, stack)
+			if err != nil {
+				return 0, err
+			}
+		default:
+			digit, err := strconv.ParseFloat(s, 32)
+			if err != nil {
+				return 0, err
+			}
+			stack = append(stack, float32(digit))
+		}
+	}
+	if len(stack) == 1 {
+		res, _, _ := pop(stack)
+		return res, nil
+	}
+	return 0, fmt.Errorf("Expression is not compnete and can't be parsed")
 }
--- calculate_test.go
+++ calculate_test.go
@@ -5,10 +5,29 @@ import (
 )
 
 func TestReversePolish(t *testing.T) {
-	var input = "5 1 2 + 4 * + 3 -"
-	var output = 14.0
-	var result = calculate(input)
-	if output != result {
+	checkCalculation(t, "5 1 +", 6)
+	checkCalculation(t, "5.2 1.4 +", 6.6)
+	checkCalculation(t, "2 21 * 30 -", 12)
+	checkCalculation(t, "5 1 2 + 4 * + 3 -", 14)
+}
+
+func TestReversePolishErrors(t *testing.T) {
+	checkCalculationOnError(t, "5 1 +-")
+	checkCalculationOnError(t, "5 1 + -")
+	checkCalculationOnError(t, "5 +")
+}
+
+func checkCalculation(t *testing.T, input string, output float32) {
+	result, err := calculate(input)
+	if err != nil {
+		t.Error(err)
+	} else if float32(output) != float32(result) {
 		t.Errorf("Get %v. But evaluation for %v should be %v", result, input, output)
+	}
+}
+func checkCalculationOnError(t *testing.T, input string) {
+	_, err := calculate(input)
+	if err == nil {
+		t.Errorf("Errors should be return for this input: %v", input)
 	}
 }
